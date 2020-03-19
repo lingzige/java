ce4e1afe9df9c995cebb2070f1a5bf20462ddc39
lingzige <1558820253@qq.com>
2020-03-19 10:48:43 +0800

    上升的温度

---
+++ mysql/上升的温度.sql
@@ -0,0 +1,25 @@
+leetcode第197题：上升的温度
+
+给定一个 Weather 表，编写一个 SQL 查询，来查找与之前（昨天的）日期相比温度更高的所有日期的 Id。
+
++---------+------------------+------------------+
+| Id(INT) | RecordDate(DATE) | Temperature(INT) |
++---------+------------------+------------------+
+|       1 |       2015-01-01 |               10 |
+|       2 |       2015-01-02 |               25 |
+|       3 |       2015-01-03 |               20 |
+|       4 |       2015-01-04 |               30 |
++---------+------------------+------------------+
+例如，根据上述给定的 Weather 表格，返回如下 Id:
+
++----+
+| Id |
++----+
+|  2 |
+|  4 |
++----+
+datediff(w2.recorddate,w1.recorddate)=1 返回两个日期之间的天数
+
+
+select w2.id from weather w1,weather w2 where datediff(w2.recorddate,w1.recorddate)=1  
+and w1.Temperature<w2.Temperature
